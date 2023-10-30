dd2f7caf0ee3906472e5524e4b8004aca726b8c8
efuentealbaCH <efuentealba.git@gmail.com>
2023-10-30 19:00:18 -0300

    feat: (permisos) configuracion de permisos para linux

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://vinculamos.localhost/'),
+    'url' => env('APP_URL', 'http://vinculamos.ufro.localhost/'),
 
     'asset_url' => env('ASSET_URL', null),
 
